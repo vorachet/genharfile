b8cbb7eb31cc23e18111b2dc29c4b7fe3ab04aa5
Vorachet Jaroensawas <vorachet@gmail.com>
2023-02-12 14:18:20 +0700

    remove unsed code

--- bin/index.js
+++ bin/index.js
@@ -9,25 +9,7 @@ const argv = require('yargs/yargs')(process.argv.slice(2))
     .describe('u', 'Website url')
     .argv;
 
-function usage() {
-    console.log(`npx genHARFile --name "Wikipedia" --url "https://www.wikipedia.org"`)
-}
-
-if (!argv.name) {
-    console.error("--name cannot be empty");
-    usage();
-    process.exit(1);
-
-}
-
-if (!argv.url) {
-    console.error("--url cannot be empty");
-    usage();
-    process.exit(1);
-}
-
 start();
-
 async function start() {
     await genHARFile({
         name: argv.name,
